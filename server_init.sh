f5642fd0d16c0bfdbc25a3121ac963f5ee910add
Joe McWilliams <accounts@joemcwilliams.com>
2012-10-19 21:25:17 -0400

    don't do sudo -i
    we will just run it as root

---
+++ server_init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+wget http://apt.puppetlabs.com/puppetlabs-release-quantal.deb
+dpkg -i puppetlabs-release-quantal.deb
+apt-get update
+apt-get install puppet git-core
+cd /etc/puppet/modules
+git clone git://github.com/example42/puppet-vim.git vim
+git clone git://github.com/example42/puppet-nginx.git nginx
+git clone git://github.com/puppetlabs/puppetlabs-stdlib.git stdlib
+git clone git://github.com/josephmc5/puppet-dropbox.git dropbox
+git clone git://github.com/plathrop/puppet-module-supervisor.git supervisor
