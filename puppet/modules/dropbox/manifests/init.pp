0e16cbf073e984c947a65460a52993c8b81b50fb
Joe McWilliams <josephmc5@gmail.com>
2012-10-21 08:30:40 -0400

    removed puppet modules

--- puppet/modules/dropbox/manifests/init.pp
+++
@@ -1,9 +0,0 @@
-class dropbox {
-  include stdlib
-  include dropbox::config
-
-  anchor { 'dropbox::begin': }
-  -> class { 'dropbox::package': }
-  ~> class { 'dropbox::service': }
-  -> anchor { 'dropbox::end': }
-}
--- puppet/modules/dropbox/manifests/params.pp
+++
@@ -1,5 +0,0 @@
-class dropbox::params {
-  $dx_uid = 'dropbox'
-  $dx_gid = 'dropbox'
-  $dx_home = '/opt/dropbox'
-}
